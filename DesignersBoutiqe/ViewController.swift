a9055f2f462f4dcc0bd96bf26ec2557d62fc2505
“shorooq” <shorooqalbahri2@gmail.com>
2020-05-02 23:57:26 +0400

    Initial Commit

---
+++ DesignersBoutiqe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesignersBoutiqe
+//
+//  Created by OJP on 5/2/20.
+//  Copyright © 2020 OJP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
